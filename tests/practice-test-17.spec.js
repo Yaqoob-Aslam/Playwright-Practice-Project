80a12838f193428377e757149fb48d06b7ed5375
Muhammad Yaqoob <reach.yaqoob@gmail.com>
2025-12-14 23:50:19 +0500

    Updated code

--- tests/practice-test-17.spec.js
+++ tests/practice-test-17.spec.js
@@ -15,15 +15,15 @@ test('Interact with frames', async ({}, testInfo) => {
   const page = await context.newPage();
 
     await page.goto("https://www.lambdatest.com/selenium-playground/generate-file-to-download-demo");
-    await page.type("#textbox", "Like, Share, comment & subs");
+    await page.type("#textbox", "Like, Share, comment & subscribe to LambdaTest");
     await page.click("#create");
     // await page.click("#link-to-download");
 
-    const [download] = await Promise.all([
+    const download = await Promise.all([
       page.waitForEvent('download'),
       page.click("#link-to-download"),
     ])
-    const downloadPath = await download.path();
-    console.log("File downloaded to: ", downloadPath);
-    await browser.close();
+
+    const fileName = download[0].suggestedFilename()
+    await download[0].saveAs(fileName);
 });
