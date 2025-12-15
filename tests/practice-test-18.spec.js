af020d6c6dc649b69910da1922da9f33909b4a18
Muhammad Yaqoob <reach.yaqoob@gmail.com>
2025-12-15 23:35:57 +0500

    Updated code

--- tests/practice-test-18.spec.js
+++ tests/practice-test-18.spec.js
@@ -1,7 +1,9 @@
+
 import { test, expect, chromium } from '@playwright/test';
 
 test('File-Upload-Interaction', async ({}, testInfo) => {
-  testInfo.setTimeout(90000); // Increase timeout to 90 seconds
+  testInfo.setTimeout(90000);
+
   const browser = await chromium.launch({
     headless: false,
     args: ['--start-maximized'],
@@ -13,10 +15,19 @@ test('File-Upload-Interaction', async ({}, testInfo) => {
   });
 
   const page = await context.newPage();  
-  await page.goto("https://blueimp.github.io/jQuery-File-Upload/")
-  await page.locator('[name="files[]"]').setInputFiles(['./tests/uploadedfile/apple-1.jpg', './tests/uploadedfile/mango-1.jpg']);
+  await page.goto("https://blueimp.github.io/jQuery-File-Upload/");
 
+  const addFilesButton = page.locator('span.btn:has-text("Add files...") >> visible=true');
+  await addFilesButton.waitFor({ state: 'visible' });
+
+  const [fileChooser] = await Promise.all([
+    page.waitForEvent('filechooser'),
+    addFilesButton.click()
+  ]);
+
+  console.log('Multiple allowed:', fileChooser.isMultiple()); 
+
+  await fileChooser.setFiles(['./tests/uploadedfile/apple-1.jpg','./tests/uploadedfile/mango-1.jpg']);
   await page.waitForTimeout(3000);
   await page.pause();
-
 });
